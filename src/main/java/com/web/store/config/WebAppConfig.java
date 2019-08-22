2023a9ae7196e067ab303b278a6a0a88defaaabf
PeterChao <kiraychao@gmail.com>
2019-08-22 09:57:02 +0800

    新增系統常數外部化、加入靜態資源功能

--- src/main/java/com/web/store/config/WebAppConfig.java
+++ src/main/java/com/web/store/config/WebAppConfig.java
@@ -1,11 +1,14 @@
 package com.web.store.config;
 
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
@@ -22,6 +25,25 @@ public class WebAppConfig extends WebMvcConfigurerAdapter {
 		resolver.setSuffix(".jsp");
 		return resolver;
 	}
+	
+	@Bean
+	public MessageSource messageSource() {
+		ResourceBundleMessageSource resource = new ResourceBundleMessageSource();
+		resource.setBasename("messages");
+		return resource;
+	}
+	
+	@Override 
+	// 應用系統必須透過ResourceHandlerRegistry的輔助，以便處理靜態檔案：
+	// 只要請求路徑是以/css/開頭的任何請求，都轉到/WEB-INF/views/css/去尋找
+	// 只要請求路徑是以/image/開頭的任何請求，都轉到/WEB-INF/views/images/去尋找
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+	     registry.addResourceHandler("/css/**")
+	             .addResourceLocations("/WEB-INF/views/css/");
+	     registry.addResourceHandler("/image/**")
+	             .addResourceLocations("/WEB-INF/views/images/");
+	}
+	
 	@Bean
 	public CommonsMultipartResolver multipartResolver() {
 		CommonsMultipartResolver resolver = new CommonsMultipartResolver();
