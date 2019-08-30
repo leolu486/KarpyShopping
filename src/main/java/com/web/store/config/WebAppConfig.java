b16f300ebfe612f0256559ef2eba52dab9df8e71
chian <moghost520@gmail.com>
2019-08-30 17:11:00 +0800

    新增首頁

--- src/main/java/com/web/store/config/WebAppConfig.java
+++ src/main/java/com/web/store/config/WebAppConfig.java
@@ -7,16 +7,18 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
+
 @Configuration
 @EnableWebMvc
 @ComponentScan("com.web.store")
 public class WebAppConfig extends WebMvcConfigurerAdapter {
-	
+
 	@Bean
 	public ViewResolver internalResourceViewResolver() {
 		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
@@ -25,25 +27,28 @@ public class WebAppConfig extends WebMvcConfigurerAdapter {
 		resolver.setSuffix(".jsp");
 		return resolver;
 	}
-	
+
 	@Bean
 	public MessageSource messageSource() {
 		ResourceBundleMessageSource resource = new ResourceBundleMessageSource();
 		resource.setBasename("messages");
 		return resource;
 	}
-	
-	@Override 
+
+	@Override
 	// 應用系統必須透過ResourceHandlerRegistry的輔助，以便處理靜態檔案：
 	// 只要請求路徑是以/css/開頭的任何請求，都轉到/WEB-INF/views/css/去尋找
 	// 只要請求路徑是以/images/開頭的任何請求，都轉到/WEB-INF/views/images/去尋找
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-	     registry.addResourceHandler("/css/**")
-	             .addResourceLocations("/WEB-INF/views/css/");
-	     registry.addResourceHandler("/images/**")
-	             .addResourceLocations("/WEB-INF/views/images/");
-	     registry.addResourceHandler("/js/**")
-         .addResourceLocations("/WEB-INF/views/js/");
+		registry.addResourceHandler("/css/**").addResourceLocations("/WEB-INF/views/css/");
+		registry.addResourceHandler("/images/**").addResourceLocations("/WEB-INF/views/images/");
+//		registry.addResourceHandler("/js/**").addResourceLocations("/WEB-INF/views/js/");
+	}
+
+	
+	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer)
+	{
+		configurer.enable();
 	}
 	
 	@Bean
